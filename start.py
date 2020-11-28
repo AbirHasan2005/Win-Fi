c6737b964fdad3ba01c25ae8f4cb34305f9bd264
Abir Hasan <hackitabirhasan@gmail.com>
2020-11-28 23:39:06 +0600

    Update start.py

--- start.py
+++ start.py
@@ -15,12 +15,13 @@ for name in router:
     passwrd = [line.split(':')[1][1:-1] for line in info if "Key Content" in line]
     net = [line.split(':')[1][1:-1] for line in info if "Network type" in line]
     version = [line.split(':')[1][1:-1] for line in info if "Version" in line]
+    sec = [line.split(':')[1][1:-1] for line in info if "Authentication" in line]
     try:
         # Output with Password
-        print(f' Router   : {name} \n Password : {passwrd[0]} \n Network  : {net[0]} \n Version  : {version[0]} \n')
+        print(f' Router   : {name} \n Password : {passwrd[0]} \n Network  : {net[0]} \n Version  : {version[0]} \n Security : {sec[0]} \n')
     except IndexError:
         # Output without Password
-        print(f' Router   : {name} \n Password : Not Found! \n Version  : {version[0]} \n')
+        print(f' Router   : {name} \n Password : Not Found! \n Network  : {net[0]} \n Version  : {version[0]} \n Security : {sec[0]} \n')
 
 ########## Main Code ############# Main Code ############# Main Code ##########
 
