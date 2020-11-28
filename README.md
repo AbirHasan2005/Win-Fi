b2d98e673a16719b1511d40e0a6e5f49fd1bfe26
Abir Hasan <hackitabirhasan@gmail.com>
2020-11-28 22:46:23 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Win-Fi v0.7-Alpha
+
+This is a Python3 script which shows saved WiFi Password & other details based on `netsh`.
+
+#### This is in Development !!
---
+++ start.py
@@ -0,0 +1,25 @@
+import subprocess
+
+print("")
+print("Wi-Fi Router Names & Passwords:")
+print("")
+
+########## Main Code ############# Main Code ############# Main Code ##########
+
+data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+router = [line.split(':')[1][1:-1] for line in data if "All User Profile" in line]
+
+
+for name in router:
+    info = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', name, 'key=clear']).decode('utf-8').split('\n')
+    passwrd = [line.split(':')[1][1:-1] for line in info if "Key Content" in line]
+    try:
+        # Output with Password
+        print(f' Router   : {name} \n Password : {passwrd[0]} \n')
+    except IndexError:
+        # Output without Password
+        print(f' Router   : {name} \n Password : Not Found! \n')
+
+########## Main Code ############# Main Code ############# Main Code ##########
+
+print("      Coded By AbirHasan2005      ")
