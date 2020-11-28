ebf212f44d7a78e7f54c9263b4477dc5dd67bf94
Abir Hasan <hackitabirhasan@gmail.com>
2020-11-28 23:07:56 +0600

    Update start.py

--- start.py
+++ start.py
@@ -14,12 +14,13 @@ for name in router:
     info = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', name, 'key=clear']).decode('utf-8').split('\n')
     passwrd = [line.split(':')[1][1:-1] for line in info if "Key Content" in line]
     net = [line.split(':')[1][1:-1] for line in info if "Network type" in line]
+    version = [line.split(':')[1][1:-1] for line in info if "Version" in line]
     try:
         # Output with Password
-        print(f' Router   : {name} \n Password : {passwrd[0]} \n Network  : {net[0]} \n')
+        print(f' Router   : {name} \n Password : {passwrd[0]} \n Network  : {net[0]} \n Version  : {version[0]} \n')
     except IndexError:
         # Output without Password
-        print(f' Router   : {name} \n Password : Not Found! \n')
+        print(f' Router   : {name} \n Password : Not Found! \n Version  : {version[0]} \n')
 
 ########## Main Code ############# Main Code ############# Main Code ##########
 
